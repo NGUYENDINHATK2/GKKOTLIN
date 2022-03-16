aa81992f5ef7d737ce6e3d6eaed193dffba08b93
Nguyễn Đình Át <84607512+NGUYENDINHATK2@users.noreply.github.com>
2022-03-16 07:31:01 +0700

    Update RecyclerAdapter.kt

--- app/src/main/java/com/example/nguyendinhat_20it536/RecyclerAdapter.kt
+++ app/src/main/java/com/example/nguyendinhat_20it536/RecyclerAdapter.kt
@@ -18,7 +18,7 @@ class RecyclerAdapter(private val mList: List<ItemsViewModel>): RecyclerView.Ada
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerAdapter.ViewHolder {
         val v= LayoutInflater.from(parent.context).inflate(R.layout.card_layout,parent,false)
 
-        v.setBackgroundColor(Color.argb( Random.nextInt(0, 1000),  Random.nextInt(0, 1000),  Random.nextInt(0, 1000),  Random.nextInt(0, 1000)))
+        
         return  ViewHolder(v)
     }
 
